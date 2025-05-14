1bc71ad9becd85dc59b3fcd2003b9a5f9fcf0e96
jilian <hernandezjilian1@gamil.com>
2025-05-14 13:20:12 -0500

    cambios

--- Tablas_creadad_ejercicio_1.sql
+++ Tablas_creadad_ejercicio_1.sql
@@ -17,52 +17,6 @@ ALTER TABLE producto
 ADD CONSTRAINT fk_fabricante_producto
 FOREIGN KEY (codigo_fabricante) REFERENCES fabricante (codigo);
 
-SELECT 
-  p.nombre AS nombre_producto,
-  p.precio,
-  f.nombre AS nombre_fabricante
-FROM producto p
-JOIN fabricante f 
-  ON p.codigo_fabricante = f.codigo;
+SELECT nombre FROM producto;
 
-SELECT 
-  p.nombre AS nombre_producto,
-  p.precio,
-  f.nombre AS nombre_fabricante
-FROM producto p
-LEFT JOIN fabricante f 
-  ON p.codigo_fabricante = f.codigo;
-
-SELECT 
-  p.nombre AS nombre_producto,
-  p.precio,
-  f.nombre AS nombre_fabricante
-FROM producto p
-RIGHT JOIN fabricante f 
-  ON p.codigo_fabricante = f.codigo;
-
-SELECT 
-  p.nombre AS nombre_producto,
-  p.precio,
-  f.nombre AS nombre_fabricante
-FROM producto p
-LEFT JOIN fabricante f 
-  ON p.codigo_fabricante = f.codigo
-
-UNION
-
-SELECT 
-  p.nombre AS nombre_producto,
-  p.precio,
-  f.nombre AS nombre_fabricante
-FROM producto p
-RIGHT JOIN fabricante f 
-  ON p.codigo_fabricante = f.codigo;
-
-SELECT 
-  f.nombre AS nombre_fabricante,
-  COUNT(p.codigo) AS total_productos
-FROM fabricante f
-LEFT JOIN producto p 
-  ON p.codigo_fabricante = f.codigo
-GROUP BY f.nombre;
+SELECT * FROM producto;
